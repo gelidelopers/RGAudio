4aeccadbf65b3807d6400156e246b5d955c425b2
barrien2 <xavib99@gmail.com>
2019-02-15 19:51:10 +0100

    Crear control de llista drag and drop
    Falta comprovar funcionalitat

---
+++ ControlsLib/DropBetweenList.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ControlsLib
+{
+    partial class MusicLibrary
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(415, 528);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListView1_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListView1_DragEnter);
+            // 
+            // MusicLibrary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listView1);
+            this.Name = "MusicLibrary";
+            this.Size = new System.Drawing.Size(415, 528);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ ControlsLib/DropBetweenList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlsLib
+{
+    public partial class MusicLibrary : UserControl
+    {
+        public MusicLibrary()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ListViewItem)))
+            {
+                var item = (ListViewItem)e.Data.GetData(typeof(ListViewItem));
+                
+                    e.Effect = DragDropEffects.Move;
+                
+            }
+        }
+
+        private void ListView1_DragDrop(object sender, DragEventArgs e)
+        {
+            var item = (ListViewItem)e.Data.GetData(typeof(ListViewItem));
+            item.ListView.Items.Remove(item);
+            listView1.Items.Add(item);
+        }
+    }
+}
